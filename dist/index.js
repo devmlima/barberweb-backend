c3e8590290940937b82258cd74d090a67892855e
Matheus Lima <matheus.lima@squadra.com.br>
2022-04-02 10:29:50 -0300

    remove dist

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const app = new app_1.App();
-app.getApp();
-//# sourceMappingURL=index.js.map
